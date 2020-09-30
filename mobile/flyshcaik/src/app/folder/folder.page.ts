3bd7a77e2e252c9e632c6e7755da63d57935624d
redeemer matshili <48877861+redeemer5@users.noreply.github.com>
2020-09-30 15:09:08 +0200

    latest app

--- mobile/flyshcaik/src/app/folder/folder.page.ts
+++ mobile/flyshcaik/src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {HttpService} from '../services/http.service';
 
 @Component({
   selector: 'app-folder',
@@ -9,10 +10,16 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute,public http:HttpService) { }
+
+  books;
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.http.viewAllBooks().subscribe(http=>{
+      this.books = http;
+    })
   }
 
   name(id)
